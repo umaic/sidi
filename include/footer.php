a4265807e655341f1620ea9f339d3f9faabf4a0e
yecarrillo <yecarrillo@gmail.com>
2016-10-13 16:12:30 -0500

    Agregar logo de IMMAP

--- include/footer.php
+++ include/footer.php
@@ -4,6 +4,9 @@
             <img src="/sissh/images/umaic/logo_OCHA.png" alt="OCHA Colombia" />
         </div>
         <div class="logo">
+            <img src="/sissh/images/umaic/logo_IMMAP.png" alt="IMMAP Inc." />
+        </div>
+        <div class="logo">
             <img src="/sissh/images/umaic/logo_PNUD.jpg" alt="PNUD Colombia" />
         </div>
         <div class="logo">
